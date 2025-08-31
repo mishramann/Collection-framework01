bdd3ae355a4700a40029f6d03a7d38adde944fae
mishramann <piyushmishra08oct@gmail.com>
2025-08-31 15:07:43 +0530

    learn arraylist

---
+++ Arraylist.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Arraylist {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+    // int x;
+    list.add(12);// 0
+    list.add(2);// 1
+    list.add(3);// 2
+    list.add(5);// 3
+    // System.out.println(list.get(2));
+    // System.out.println(list.size());
+    // for (i = 0; i < list.size(); i++) {
+    // // System.out.println(list.get(i));
+    // }
+
+    // for (int x : list) {
+    // // System.out.println(x);
+    // }
+
+    // method for check existence
+    // System.out.println(list.contains(5));
+    // System.out.println(list.contains(50));
+
+    // remove the element of index present
+    // list.remove(2);
+
+    // list.add(2, 50);
+    // list.set(2, 40); //remove the element of index replace by new value
+    for (int x : list) {
+      System.out.println(x); // if we intitialize an arraylist with the capacity of 12 element then if
+                             // inceases size of that arraylist it will increase 1.5 times of that arraylist
+    }
+
+  }
+}
